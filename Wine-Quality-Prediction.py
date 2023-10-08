a67c9427bfc519ca85db47d5746e90f56944512e
prithijack <103311118+prithijack@users.noreply.github.com>
2023-10-09 03:00:18 +0530

    Add files via upload

---
+++ Wine-Quality-Prediction.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
+
+url = "https://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-red.csv"
+data = pd.read_csv(url, sep=';')
+
+data.isnull().sum()
+X = data.drop('quality', axis=1)
+y = data['quality']
+
+
+# In[2]:
+
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
+mse = mean_squared_error(y_test, y_pred)
+r2 = r2_score(y_test, y_pred)
+
+print("Mean Squared Error:", mse)
+print("R-squared:", r2)
+
+plt.scatter(y_test, y_pred)
+plt.xlabel("Actual Wine Quality")
+plt.ylabel("Predicted Wine Quality")
+plt.title("Actual vs. Predicted Wine Quality")
+plt.show()
+
+
+# In[ ]:
+
+
+
+
